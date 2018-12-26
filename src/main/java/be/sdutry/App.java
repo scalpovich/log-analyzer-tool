d35c6e2d47de9e061f985c0c842c8d360d363f7e
Stefaan Dutry <stefaan.dutry@gmail.com>
2018-12-26 19:39:42 +0100

    added main class logic

--- src/main/java/be/sdutry/App.java
+++ src/main/java/be/sdutry/App.java
@@ -1,13 +1,29 @@
 package be.sdutry;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import be.sdutry.processor.LogFileProcessor;
+import be.sdutry.model.output.Rendering;
+import be.sdutry.model.output.Report;
+import be.sdutry.util.ReportSerializer;
+
+import java.io.File;
+import java.util.List;
+
+public class App {
+    public static void main( String[] args ) {
+        File logFile = new File(args[0]);
+        File outputFile = new File(args[1]);
+
+        LogFileProcessor logFileProcessor = new LogFileProcessor(logFile);
+        List<Rendering> renderings = logFileProcessor.processRenderings();
+
+        Report report = new Report(renderings);
+
+        ReportSerializer serializer = new ReportSerializer();
+        try {
+            serializer.serializeReport(report, outputFile);
+        } catch (final Exception exc) {
+            //TODO log and handle exception
+            throw new RuntimeException("could not generate report for logfile", exc);
+	}
     }
 }
--- src/test/java/be/sdutry/AppTest.java
+++ src/test/java/be/sdutry/AppTest.java
@@ -1,20 +1,39 @@
 package be.sdutry;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.xmlunit.matchers.CompareMatcher.isSimilarTo;
 
+import java.io.File;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import org.xmlunit.builder.Input;
+
+public class AppTest {
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testEmptyInputFile() throws Exception {
+        File inputFile = new File(this.getClass().getResource("/empty.log").getFile());
+        File outputFile = folder.newFile("testEmptyOutput.xml");
+        String[] args = new String[] {inputFile.getAbsolutePath(), outputFile.getAbsolutePath()};
+
+        App.main(args);
+
+        assertThat(Input.from(outputFile), isSimilarTo(Input.fromStream(getClass().getResourceAsStream("/emptyReport.xml"))).ignoreWhitespace());
+    }
+
+    @Test
+    public void testExampleInputFile() throws Exception {
+        File inputFile = new File(this.getClass().getResource("/example2.log").getFile());
+        File outputFile = folder.newFile("testExample2Output.xml");
+        String[] args = new String[] {inputFile.getAbsolutePath(), outputFile.getAbsolutePath()};
+
+        App.main(args);
+
+        assertThat(Input.from(outputFile), isSimilarTo(Input.fromStream(getClass().getResourceAsStream("/example2Report.xml"))).ignoreWhitespace());
     }
 }
